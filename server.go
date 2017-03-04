74350fb1c648a05006f7b5f71248870c76e4c1dc
roland-hochmuth <roland.hochmuth@hpe.com>
2017-03-04 12:30:14 -0700

    Initial commit of simple kafka consumer capable of unmarshaling and printing metrics

---
+++ server.go
@@ -0,0 +1,105 @@
+// Copyright 2017 Hewlett Packard Enterprise Development LP
+//
+//    Licensed under the Apache License, Version 2.0 (the "License"); you may
+//    not use this file except in compliance with the License. You may obtain
+//    a copy of the License at
+//
+//         http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+//    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+//    License for the specific language governing permissions and limitations
+//    under the License.
+
+package main
+
+import (
+	log "github.com/Sirupsen/logrus"
+	"os"
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"fmt"
+	"os/signal"
+	"syscall"
+	"github.hpe.com/UNCLE/monasca-aggregation/models"
+	"encoding/json"
+)
+
+func initLogging() {
+	// Log as JSON instead of the default ASCII formatter.
+	log.SetFormatter(&log.JSONFormatter{})
+	log.SetOutput(os.Stdout)
+	log.SetLevel(log.InfoLevel)
+}
+
+func main() {
+	initLogging()
+
+	if len(os.Args) < 4 {
+		fmt.Fprintf(os.Stderr, "Usage: %s <broker> <group> <topics..>\n",
+			os.Args[0])
+		os.Exit(1)
+	}
+
+	broker := os.Args[1]
+	group := os.Args[2]
+	topics := os.Args[3:]
+
+	sigchan := make(chan os.Signal)
+	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
+
+	c, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers":               broker,
+		"group.id":                        group,
+		"session.timeout.ms":              6000,
+		"go.events.channel.enable":        true,
+		"go.application.rebalance.enable": true,
+		"default.topic.config":            kafka.ConfigMap{"auto.offset.reset": "earliest"}})
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to create consumer: %s\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Created Consumer %v\n", c)
+
+	err = c.SubscribeTopics(topics, nil)
+
+	run := true
+
+	for run == true {
+		select {
+		case sig := <-sigchan:
+			fmt.Printf("Caught signal %v: terminating\n", sig)
+			run = false
+
+		case ev := <-c.Events():
+			switch e := ev.(type) {
+			case kafka.AssignedPartitions:
+				fmt.Fprintf(os.Stderr, "%% %v\n", e)
+				c.Assign(e.Partitions)
+			case kafka.RevokedPartitions:
+				fmt.Fprintf(os.Stderr, "%% %v\n", e)
+				c.Unassign()
+			case *kafka.Message:
+				metricEnvelope := models.MetricEnvelope{}
+				err = json.Unmarshal([]byte(e.Value), &metricEnvelope)
+				if err != nil {
+					fmt.Printf("%% Message on %s:\n%s\n",
+						e.TopicPartition, string(e.Value))
+					panic(err.Error())
+				} else {
+					fmt.Print(metricEnvelope)
+				}
+			case kafka.PartitionEOF:
+				fmt.Printf("%% Reached %v\n", e)
+			case kafka.Error:
+				fmt.Fprintf(os.Stderr, "%% Error: %v\n", e)
+				run = false
+			}
+		}
+	}
+
+	fmt.Printf("Closing consumer\n")
+	c.Close()
+}
