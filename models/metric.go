42eeb9164dc9e82a1836a3bcd0d256be26317772
roland-hochmuth <roland.hochmuth@hpe.com>
2017-03-04 21:26:03 -0700

    Formatting

--- models/metric.go
+++ models/metric.go
@@ -15,9 +15,9 @@
 package models
 
 type Metric struct {
-	Name       string         `json:"name"`
+	Name       string            `json:"name"`
 	Dimensions map[string]string `json:"dimensions"`
-	Timestamp  int64 `json:"timestamp"`
-	Value      float64 `json:"value"`
-	ValueMeta  map[string]string `json:value_meta`
+	Timestamp  int64             `json:"timestamp"`
+	Value      float64           `json:"value"`
+	ValueMeta  map[string]string `json:"value_meta"`
 }
--- models/metric_envelope.go
+++ models/metric_envelope.go
@@ -15,7 +15,7 @@
 package models
 
 type MetricEnvelope struct {
-	Metric       Metric         `json:"metric"`
-	Meta         map[string]string `json:"meta"`
-	CreationTime int64           `json:"creation_time"`
+	Metric       Metric             `json:"metric"`
+	Meta         map[string]string  `json:"meta"`
+	CreationTime int64              `json:"creation_time"`
 }
